896e5ebe653fb1bc1993cf4dcfbb6d8ec5095d65
Molati <86184198+Molati@users.noreply.github.com>
2022-07-20 14:06:00 -0700

    commit config file

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,8 @@ import NavHeader from "./Components/Navigation/NavHeader";
 function App() {
   return (
     <Container>
-      <NavHeader />;
-      <Text noOfLines={[1, 2, 3]}>
-        The quick brown fox jumps over the lazy dog is an English-language
-        pangram—a sentence that contains all of the letters of the English
-        alphabet. Owing to its existence, Chakra was created.
-      </Text>
+      <NavHeader />
+      <Text noOfLines={[1, 2, 3]}>Firesms</Text>
     </Container>
   );
 }
