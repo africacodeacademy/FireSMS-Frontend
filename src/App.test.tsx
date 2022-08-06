9383f22b1634e81093a890758af5ee058fd23cf1
Molati <86184198+Molati@users.noreply.github.com>
2022-08-06 14:05:48 -0700

    commit update tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,6 +3,6 @@ import App from "./App";
 
 test("renders firesms app", () => {
   render(<App />);
-  const title = screen.getByText(/FireSMS/i);
+  const title = screen.getAllByText(/FireSMS/i)[0] as HTMLAnchorElement;
   expect(title).toBeInTheDocument();
 });
