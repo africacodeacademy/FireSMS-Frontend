fe9a1a232bb587ace37ea96c128843fd4fc1f25a
Molati <86184198+Molati@users.noreply.github.com>
2022-08-07 15:37:19 -0700

    commit update test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,6 +3,8 @@ import Footer from "./Components/Footer/footer";
 
 test("renders firesms Footer", () => {
   render(<Footer />);
-  const title = screen.getAllByText(/FireSMS/i)[0] as HTMLAnchorElement;
+  const title = screen.getAllByText(
+    /Powered by Africa Code Foundry/i,
+  )[0] as HTMLAnchorElement;
   expect(title).toBeInTheDocument();
 });
