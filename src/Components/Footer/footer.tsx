36e9a0a716a71abc955b1719ed0991eaef8d8815
Molati <86184198+Molati@users.noreply.github.com>
2022-10-14 11:19:55 -0700

    commit update footer size

--- src/Components/Footer/footer.tsx
+++ src/Components/Footer/footer.tsx
@@ -3,7 +3,7 @@ import { Box, Text } from "@chakra-ui/react";
 function Footer() {
   return (
     <Box
-      pt={{ base: "8%", md: "2%" }}
+      pt={{ base: "8.1%", md: "2%" }}
       pb={{ base: "8%", md: "2%" }}
       mb={{ base: "0%", md: "0%" }}
       borderColor="gray.800"
